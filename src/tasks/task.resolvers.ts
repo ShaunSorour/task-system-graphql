791470a0c99820d485ef66ec549d603a8e03b68b
Shaun <swsorour8@gmail.com>
2023-09-20 10:13:47 +0200

    create task

---
+++ src/tasks/task.resolvers.ts
@@ -0,0 +1,31 @@
+import { GraphQLError } from "graphql";
+import { Resolvers } from '../__generated__/resolvers-types'
+import { taskService } from "./task.service";
+
+
+interface MyContext {
+    authorized: boolean;
+}
+
+const checkAuthorization = (context: MyContext) => {
+    if (!context.authorized) {
+        throw new GraphQLError('Unauthorized', {
+            extensions: { code: 'UNAUTHORIZED' }
+        });
+    }
+}
+
+export const taskResolvers: Resolvers = {
+    Mutation: {
+        async createTask(parent, { input }, context) {
+            checkAuthorization(context);
+            const task = await taskService.createTask(input, context.currentUser.userId);
+
+            return task;
+        }
+    },
+
+    Query: {
+
+    }
+}
---
+++ src/tasks/task.service.ts
@@ -0,0 +1,25 @@
+import { Repository } from 'typeorm'
+import { CreateTaskInput } from 'src/__generated__/resolvers-types';
+import { AppDataSource } from '../auth/user/app-data.source';
+import { Task } from './task/entity/task.entity';
+import { userService } from '../auth/user/user.service';
+import { GraphQLError } from 'graphql';
+
+
+export class TaskService {
+    constructor(
+        public taskRepository: Repository<Task>
+    ) { }
+
+    async createTask(createTaskInput: CreateTaskInput, id: number) {
+        const user = await userService.getUserById(id);
+        if (!user) { throw new GraphQLError('User not found'); }
+
+        const task = this.taskRepository.create({ ...createTaskInput })
+        task.user = user
+
+        return await this.taskRepository.save(task)
+    }
+}
+
+export const taskService = new TaskService(AppDataSource.getRepository(Task))
